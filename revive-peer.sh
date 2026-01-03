03d4124ed37498d514df88a21430140cadcb292b
Ragul <ragul.harisankar@gmail.com>
2026-01-03 12:38:48 +0530

    Added revive feature & script

---
+++ revive-peer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wg set wg0 peer $public_key endpoint 192.168.52.134:5182 allowed-ips $address/32 persistent-keepalive 25
+echo {\"private_key\": \"$private_key\", \"public_key\": \"$public_key\"}
