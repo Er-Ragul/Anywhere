58165b2fcb33fd5ce9b7773ed7c0ab66780f03af
Ragul <ragul.harisankar@gmail.com>
2026-01-05 13:43:12 +0530

    Fixed authentication & script bugs

--- add-peer.sh
+++ add-peer.sh
@@ -2,5 +2,5 @@
 
 private_key="$(wg genkey)"
 public_key="$(echo $private_key | wg pubkey)"
-wg set wg0 peer $public_key endpoint 192.168.52.134:5182 allowed-ips $address/32 persistent-keepalive 25
+wg set wg0 peer $public_key endpoint 192.168.52.134:51820 allowed-ips $address/32 persistent-keepalive 25
 echo {\"private_key\": \"$private_key\", \"public_key\": \"$public_key\"}
